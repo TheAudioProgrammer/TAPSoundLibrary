89b4cbb41575964d5a3056137361cdb66fe5ced0
Claudio Stevanato <clau_ste@yahoo.it>
2018-02-11 23:21:29 +0100

    Add files via upload

---
+++ Oscillators/WaveTable.cpp
@@ -0,0 +1,129 @@
+/*
+  ==============================================================================
+
+    WaveTable.cpp
+    Created: 11 Feb 2018 11:19:46pm
+    Author:  Claudio
+
+  ==============================================================================
+*/
+
+#include "WaveTable.h"
+
+#include <algorithm>
+
+
+WaveTable::WaveTable()
+{
+}
+
+
+WaveTable::WaveTable(int length)
+{
+	content = std::vector<float>(length);
+}
+
+WaveTable::~WaveTable()
+{
+}
+
+void WaveTable::setLength(int length)
+{
+	//We create a vector of the current length;
+	std::vector<float> newContent(length);
+	int currentLength = getLength();
+	//If our current content at least has 1 value or more
+	if (currentLength > 0) {
+
+	}
+}
+
+void WaveTable::cutAboveRange(float range)
+{
+	if (range >= 0.0) {
+		//We loop through the wavetable
+		for (int i = 0; i < getLength(); i++) {
+			//We get the current value of the wavetable with content.at[i] where "i" is an integer
+			//If the current value exceeds the range we can set the value to take tha range value
+			if (content.at[i] > range) {
+				content.at[i] = range;
+			}
+			if (content.at[i] < (range * -1)) {
+				content.at[i] = range * -1;
+			}
+		}
+	}
+}
+
+std::vector<float> WaveTable::readCutAboveRange(float range)
+{
+	std::vector<float> readWave = content;
+	if (range >= 0.0) {
+		for (int i = 0; i < readWave.size(); i++) {
+			//We get the current value of the wavetable with content.at[i] where "i" is an integer
+			//If the current value exceeds the range we can set the value to take tha range value
+			if (readWave.at[i] > range) {
+				readWave.at[i] = range;
+			}
+			if (readWave.at[i] < (range * -1)) {
+				readWave.at[i] = range * -1;
+			}
+		}
+	}
+	return readWave;
+}
+
+void WaveTable::invertWavetable(float invert)
+{
+	//If our invert has a value between -1.0 and 1.0
+	if (invert >= -1.0 && invert <= 1.0) {
+		//If it equals 1.0 we will not have to affect the wavetable
+		if (invert != 1.0) {
+			//We loop through the wavetable
+			for (int i = 0; i < getLength(); i++) {
+				content.at[i] = content.at[i] * invert;
+			}
+		}
+	}
+}
+
+std::vector<float> WaveTable::readInvertWavetable(float invert)
+{
+	std::vector<float> readWave = content;
+	//If our invert has a value between -1.0 and 1.0
+	if (invert >= -1.0 && invert <= 1.0) {
+		//If it equals 1.0 we will not have to affect the wavetable
+		if (invert != 1.0) {
+			//We loop through the wavetable content and we read it
+			for (int i = 0; i < readWave.size(); i++) {
+				readWave.at[i] = readWave.at[i] * invert;
+			}
+		}
+	}
+	return readWave;
+}
+
+void WaveTable::scaleWavetable(float newMin, float newMax)
+{
+	//We get the current min and max values of our wavetable (not yet scaled)
+	float currentMin = *std::min_element(content.begin(), content.end());
+	float currentMax = *std::max_element(content.begin(), content.end());
+	//We loop through the wavetable
+	for (int i = 0; i < getLength(); i++) {
+		content.at[i] = scaleValue(content.at[i], currentMin, currentMax, newMin, newMax);
+	}
+}
+
+std::vector<float> WaveTable::readScaleWavetable(float newMin, float newMax)
+{
+	std::vector<float> readWave = content;
+	//We get the current min and max values of our read wavetable (not yet scaled)
+	float currentMin = *std::min_element(readWave.begin(), readWave.end());
+	float currentMax = *std::max_element(readWave.begin(), readWave.end());
+	//We loop through the read wavetable
+	for (int i = 0; i < readWave.size(); i++) {
+		readWave.at[i] = scaleValue(readWave.at[i], currentMin, currentMax, newMin, newMax);
+	}
+	return readWave;
+}
+
---
+++ Oscillators/WaveTable.h
@@ -0,0 +1,154 @@
+/*
+  ==============================================================================
+
+    WaveTable.h
+    Created: 11 Feb 2018 11:19:46pm
+    Author:  Claudio
+
+  ==============================================================================
+*/
+
+#pragma once
+
+//We need <vector> inclusion because the wavetable is a vector of float values;
+#include <vector>
+
+/*
+This class represent a wavetable, which, in poor words, is nothing than an array that can be read
+For example, a wavetable representing a sawtooth wave will store values from -1 to +1
+*/
+
+#pragma once
+class WaveTable
+{
+public:
+	/*
+	Constructor
+	*/
+	WaveTable();
+
+	/*
+	Constructor
+	//When we create a wavetable for the first time, we can set it's length
+	*/
+	WaveTable(int length);
+
+	/*
+	Destructor
+	*/
+	~WaveTable();
+
+	/*
+	This method add a value to our content vector
+	*/
+	void addValue(float value) { content.push_back(value); }
+
+	/*
+	This method set a value to our content vector at the index
+	*/
+	void setValue(int index, float value) { content.at[index] = value; }
+
+	/*
+	This method returns the wavetable vector length
+	*/
+	int getLength() const { return content.size(); }
+
+	/*
+	_______________________________________ TODO __________________________________________________________________________________________
+
+	This method will stretch the wavetable with the passed length
+	Pratically the wavetable will remains the same but it will have a different length
+
+	Example: I have this wavetable where content has 512 samples, and it's a sawtooth wave
+	I call setLength(1024)
+	The content will now be a vector of 1024 samples, but it will still be a sawtooth wave
+	*/
+	void setLength(int length);
+
+	/*
+	This method set the wavetable content to equal the passed vector
+	*/
+	void setContent(std::vector<float> contentVector) { content = contentVector; }
+
+	/*
+	This method simply return the wavetable content
+	*/
+	std::vector<float> getContent() const { return content; }
+
+	/*
+	This function will cut everything from the content vector which is above the passed range
+	For example, if we have a wavetable that goes from -1.5 to +1.5 and we call "cutAboveRange(1.0f);" we will
+	ensure that all the values of the wavetable will be between -1.0 and +1.0
+	This means that this method replace with the passed value what it finds exceeding the range
+
+	REQUIREMENT: the variable range must be positive
+	*/
+	void cutAboveRange(float range);
+
+	/*
+	This function will not affect the wavetable but will only read it values and will return the cutted version
+
+	REQUIREMENT: the variable range must be positive
+	*/
+	std::vector<float> readCutAboveRange(float range);
+
+	/*
+	This function will invert every value of the wavetable
+	By passing -1.0 we will have a completely inverted wavetable
+	By passing 1.0 we will not affect the wavetable
+	By passing 0.0 we will set the whole wavetable to be just 0 values
+	By passing a value different than the one just explained, we will invert the wavetable by a value
+
+	REQUIREMENT: the variable invert must be between -1.0 and 0.0
+	*/
+	void invertWavetable(float invert);
+
+	/*
+	This function will perform invertWavetable but will not affect the content
+
+	REQUIREMENT: the variable invert must be between -1.0 and 0.0
+	*/
+	std::vector<float> readInvertWavetable(float invert);
+
+	/*
+	This function will scale the wavetable by the passing values
+
+	If our wavetable content range is between -5 and 1
+	And we call scaleWavetable(-1.0, 1.0)
+
+	Thwe wavetable content will now be between -1 and 1 but it will contains always the same shape
+	*/
+	void scaleWavetable(float newMin, float newMax);
+
+	/*
+	This function will call scaleWavetable but will not affect the content
+	*/
+	std::vector<float> readScaleWavetable(float newMin, float newMax);
+
+	/*
+	This method simply takes a value, the current min/max values and the new min/max values
+
+	Here is an example
+
+	We have a vector of 100 elements between 0.0 and 100.0
+	We call(50.0, 0.0, 100.0, 0.0, 200.0)
+	Our returned value will be 100
+
+	This method reads the currentMin and currentMax that we have passed and it scale the value to fit to the newMin and newMax
+	Another example
+
+	We have a vector of 10 elements between -5.0 and 5.0
+	We call(5.0, -5.0, 5.0, 10.0, 20.0)
+	Our returned value will be 20.0
+	*/
+	float scaleValue(float value, float currentMin, float currentMax, float newMin, float newMax) const {
+		return (newMax - newMin) * (value - currentMin) / (currentMax - currentMin) + newMin;
+	}
+
+private:
+	/*
+	This variable is the one that will contain all the values of our wavetable
+	*/
+	std::vector<float> content;
+};
+

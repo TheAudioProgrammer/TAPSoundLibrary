117f7f5700e15e7f40968ef4919403585e717a3f
Janislav <jan.boerner@nexiles.com>
2018-02-15 18:25:43 +0100

    added doc strings and a example gain effect

--- Effects/Effect.h
+++ Effects/Effect.h
@@ -13,24 +13,73 @@
 namespace tsl {
     
     namespace audioeffects
-    
     {
+        /**
+         * An abstract class to define how to implement an audio effect.
+         */
         class Effect
         {
         public:
+            /**
+             * Constructor which takes the sampleRate and
+             * the samplesPerBlock. You need to call this
+             * in you sub class.
+             *
+             * @param sampleRate
+             * @param samplesPerBlock
+             */
             Effect(double sampleRate, int samplesPerBlock);
-            virtual ~Effect() = 0;
-            
+            /**
+             * Sets the samplerate.
+             *
+             * @param sampleRate
+             */
             void setSampleRate(double sampleRate);
+            /**
+             * Returns the samplerate the plugin is using.
+             *
+             * @return
+             */
             double getSampleRate();
-            
+            /**
+             * Sets the samples per block rate.
+             *
+             * @param samplesPerBlock
+             */
             void setSamplesPerBlock(int samplesPerBlock);
+            /**
+             * Returns the samples per block rate.
+             *
+             * @return
+             */
             int getSamplesPerBlock();
-            
-            virtual float process(float* left, float* right) = 0;
-            virtual float process(float* mono) = 0;
+            /**
+             * This is where you process the audio if your effect
+             * is stereo. You need to implement this method in your
+             * supclass.
+             *
+             * @param left
+             * @param right
+             */
+            virtual void process(float* left, float* right) = 0;
+            /**
+             * This is where you process the audio if your effect
+             * is mono. You need to implement this method in your
+             * supclass.
+             *
+             * @param mono
+             */
+            virtual void process(float* mono) = 0;
         protected:
+            /**
+             * The number of samples your system
+             * process in one second.
+             */
             double sampleRate;
+            /**
+             * How much samples are passed during any
+             * call of the proccess method.
+             */
             int samplesPerBlock;
         };
     }
---
+++ Effects/Gain.cpp
@@ -0,0 +1,44 @@
+/*
+  ==============================================================================
+
+    Gain.cpp
+    Created: 15 Feb 2018 4:18:43pm
+    Author:  Jan Börner
+
+  ==============================================================================
+*/
+
+#include "Gain.h"
+
+namespace tsl
+{
+    namespace audioeffects
+    {
+        Gain::Gain(double sampleRate, int samplesPerBlock) : Effect(sampleRate, samplesPerBlock)
+        {
+            vol = 1;
+        }
+        Gain::~Gain()
+        {
+        }
+        void Gain::process(float* left, float* right)
+        {
+            for(int i=0;i<samplesPerBlock;i++)
+            {
+                left[i] = left[i] * vol;
+                right[i] = right[i] * vol;
+            }
+        }
+        void Gain::process(float* mono)
+        {
+        }
+        void Gain::setVol(float vol)
+        {
+            this->vol = vol;
+        }
+        float Gain::getVol()
+        {
+            return vol;
+        }
+    }
+}
